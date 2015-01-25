21e1d6bf71816279d03851d2a611f3478375bbe0
John Whiddon <whiddojp@clarkson.edu>
2015-01-24 23:47:54 -0500

    Finished lab1.js
    Lab is complete.

--- lab1.js
+++ lab1.js
@@ -1,6 +1,7 @@
 
 var gl;
-var points;
+var index = 0;
+
 
 window.onload = function init()
 {
@@ -9,21 +10,28 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-
-    //
-    //  Configure WebGL
-    //
+	var vertices = new Float32Array([-0.5, -0.5, 0, 0.5, 0.5, -0.5,   -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, -0.5,   0.0, 0.75, 0.25, 0.25, -0.25, 0.25, -0.25, -0.25, -0.75, 0.0,   
+		0.0, -0.75, -0.25, -0.25, 0.25, -0.25, 0.25, 0.25, 0.75, 0.0 ]);
+    
+    //  Configure WebGL    
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     
-    //  Load shaders and initialize attribute buffers
-    
+    //  Load shaders and initialize attribute buffers    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    // Load the data into the GPU
+    // Load the data into the GPU	
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
-    // Associate our shader variables with our data buffer
+    // Associate our shader variables with our data buffer	
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );	
+	
+	canvas.addEventListener("mousedown", function(event){ index += 1; });
 
     render();
 };
@@ -31,4 +39,17 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+	
+	if (index == 1) {
+		gl.drawArrays( gl.TRIANGLE_FAN, 3, 4 );
+	}
+	else if (index == 2) {
+		gl.drawArrays( gl.TRIANGLE_STRIP, 7, 5 );
+		gl.drawArrays( gl.TRIANGLE_STRIP, 12, 5);
+	}	
+	else {
+		gl.drawArrays( gl.TRIANGLE_FAN, 0, 3 );
+		index = 0;
+	}
+	window.requestAnimFrame(render);
 }
